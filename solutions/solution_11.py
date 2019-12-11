f3d6317e9f383eb2ffc1a9c60c0a1c3c3adbc121
claudiobierig <claudiobierig@gmail.com>
2019-12-11 21:25:00 +0100

    day11.2

--- solutions/solution_11.py
+++ solutions/solution_11.py
@@ -2,9 +2,7 @@
 
 import sys
 from intcomputer import Intcomputer
-
-
-
+import matplotlib.pyplot as plt
 
 def turn(direction, output):
     if direction == 'U':
@@ -44,9 +42,12 @@ def read_input_to_list(path):
         return [int(x) for x in file.readline().split(",")]
 
 if __name__ == "__main__":
+    puzzle = sys.argv[1]
     input = read_input_to_list("input/11.txt")
     ship_color = {}
     position = (0,0)
+    if puzzle == "2":
+        ship_color[position] = 1
     direction = 'U'
     intcomputer = Intcomputer(input)
     while not intcomputer.has_finished():
@@ -58,4 +59,13 @@ if __name__ == "__main__":
         ship_color[position] = output[0]
         direction = turn(direction, output[1])
         position = get_new_position(position, direction)
-    print(len(ship_color.keys()))
+    
+    if puzzle == "1":
+        print(len(ship_color.keys()))
+    elif puzzle == "2":
+        whites = [p[0] for p in ship_color.items() if p[1] == 1]
+        x = [v[0] for v in whites]
+        y = [v[1] for v in whites]
+        plt.plot(x,y, 'ko')
+        plt.show()
+    
